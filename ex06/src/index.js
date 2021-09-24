8bcd1bb1f0f8e2ed068b7c48f1537cf62a623e23
Denan <denan.cigura@gmail.com>
2021-09-24 09:39:12 +0200

    JS Ex06 nesting for loops

---
+++ ex06/src/index.js
@@ -0,0 +1,16 @@
+// Only change code below this line
+function multiplyArrayFunction(myArray) {
+    var myArray = [];
+    var product = [];
+    var sum = [];
+    
+
+    return [product, sum];
+}
+        
+    
+// Only change code above this line
+
+
+console.log (multiplyArrayFunction()); // Change this line
+module.exports = multiplyArrayFunction;
