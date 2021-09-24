c1f6b758a0e3401abdae50b7d19f5528a2e344f0
Denan <denan.cigura@gmail.com>
2021-09-24 10:03:40 +0200

    JS EX09, falsy ejector only AC structure

---
+++ ex09/src/index.js
@@ -0,0 +1,14 @@
+// Only change code below this line
+function myBouncer(arr) {
+    var myArr = [];
+
+
+    return myArr;
+}
+        
+    
+// Only change code above this line
+
+
+console.log (myBouncer([7, "ate", "", false, 9])); // Change this line
+module.exports = myBouncer;
