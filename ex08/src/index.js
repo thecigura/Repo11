b9f05c95119b0b4d6e34f6e8a2c9491ff81a16cf
Denan <denan.cigura@gmail.com>
2021-09-24 09:56:09 +0200

    JS EX08 string mutation, only AC structure

---
+++ ex08/src/index.js
@@ -0,0 +1,14 @@
+// Only change code below this line
+function myMutation(arr) {
+    
+
+
+    return maxNumArray;
+}
+        
+    
+// Only change code above this line
+
+
+console.log (myMutation()); // Change this line
+module.exports = myMutation;
