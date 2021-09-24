5a908219c40e6ad44417332d32d890efebfdb9c1
Denan <denan.cigura@gmail.com>
2021-09-24 09:28:12 +0200

    JS Ex05

---
+++ ex04/src/index.js
@@ -0,0 +1,17 @@
+// Create a monitorsListArray array here
+var monitorsListArray = ["Apple", "Peach", "Berry"]
+// End of monitorsListArray array
+
+function myMonitorsFunction(arr) {
+    var newMonitorsList = [...arr];
+    // Only change code below this line
+    var monitorsList = [];
+        for(var i = 0; i < newMonitorsList.length; i++) {
+            monitorsList.push(newMonitorsList[i], i + 1);
+    }
+    return monitorsList;
+    // Only change code above this line
+}
+
+console.log (myMonitorsFunction(monitorsListArray)); // Change this line
+module.exports = myMonitorsFunction;
