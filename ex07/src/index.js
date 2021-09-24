0b6defaf341ab9a0cf9e6967991fac1a76f7113e
Denan <denan.cigura@gmail.com>
2021-09-24 09:48:19 +0200

    JS Ex07 - not finished for loop

---
+++ ex07/src/index.js
@@ -0,0 +1,17 @@
+// Only change code below this line
+function largestNumFromArr(arr) {
+    var maxNumArray = [];
+
+    for (var i=0; i < arr.length; i++) {
+        
+    }
+
+    return maxNumArray;
+}
+        
+    
+// Only change code above this line
+
+
+console.log(largestNumFromArr([[13,27,18,26],[4,5,1,3],[32,35,37,39],[1000,1001,857,1]]));    // Change this line
+module.exports = largestNumFromArr;
