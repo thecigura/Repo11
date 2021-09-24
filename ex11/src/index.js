05c0f8c0f4d4cd03f09555f10d85cd915697666c
Denan <denan.cigura@gmail.com>
2021-09-24 10:16:59 +0200

    JS Ex11, spliting array, only AC file structure

---
+++ ex11/src/index.js
@@ -0,0 +1,15 @@
+// Only change code below this line
+function splitArrayInGroups(arr, n) {
+    var result = [];
+
+
+
+    return result;
+}
+        
+    
+// Only change code above this line
+
+
+console.log(splitArrayInGroups([0, 1, 2, 3, 4, 5, 6], 3)); // Change this line
+module.exports = splitArrayInGroups;
