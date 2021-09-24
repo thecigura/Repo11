d4d09ed39240b86e8b3df5e7fe36781c8665f1dd
Denan <denan.cigura@gmail.com>
2021-09-24 10:12:03 +0200

    JS Ex10, splice, only AC file structure

---
+++ ex10/src/index.js
@@ -0,0 +1,15 @@
+// Only change code below this line
+function mySplice(arr1, arr2, n) {
+    var myArr1;
+    var myArr2;
+
+
+    return myArr2;
+}
+        
+    
+// Only change code above this line
+
+
+console.log(mySplice([1, 2, 3], [4, 5], 1)); // Change this line
+module.exports = mySplice;
