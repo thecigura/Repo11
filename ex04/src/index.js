08ca4b05ca00910ea57c73397c2373752163f02e
Denan <denan.cigura@gmail.com>
2021-09-24 09:32:22 +0200

    JS Ex04

--- ex04/src/index.js
+++ ex04/src/index.js
@@ -7,11 +7,10 @@ function myMonitorsFunction(arr) {
     // Only change code below this line
     var monitorsList = [];
         for(var i = 0; i < newMonitorsList.length; i++) {
-            monitorsList.push(newMonitorsList[i], i + 1);
+            monitorsList.push(newMonitorsList[i],  i + 1);
     }
     return monitorsList;
     // Only change code above this line
 }
-
 console.log (myMonitorsFunction(monitorsListArray)); // Change this line
 module.exports = myMonitorsFunction;
